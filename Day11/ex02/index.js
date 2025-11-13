cb071940e23a3bc812dc649cbb3e4940402e6a5f
watugiga <joosuck0506@gmail.com>
2025-11-13 10:51:03 +0900

    Day11

---
+++ Day11/ex02/index.js
@@ -0,0 +1,25 @@
+export default async function main() 
+{
+    console.log('hello')    
+
+    var a = 1
+
+    console.log(`a : ${a}`)
+    a= a+1
+
+    let b = 1
+    b += 5
+    console.log(`b ${b}`)
+
+    const c = 1
+    console.log(`c ${c}`)
+
+    let a1 = 1
+    console.log(`a1: ${a1}`)
+    {
+        let a1 = 3
+        a1 += 1
+        console.log(`a1 ${a1}`)
+    }
+    console.log(`a1: ${a1}`)
+}
